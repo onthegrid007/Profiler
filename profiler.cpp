689fbcdc2dbd71b7a1c9c278e81ba85576fdb8ef
onthegrid007 <peter.ferranti.dev@gmail.com>
2022-10-22 17:51:18 -0400

    .

--- profiler.cpp
+++ profiler.cpp
@@ -3,43 +3,56 @@ _SCM_CHILD_DEFINITIONS(Profiler::Session)
 
 namespace Profiler {
     Session::Session(const std::string Name) :
-        m_filePath(Name + "-Profile.json"),
-        m_fileStream(m_filePath),
-        m_isRunning(true),
-        m_thread([&](){ return this->Internal(); }) {}
+        m_name(Name),
+        m_mtx(new std::mutex()),
+        m_sem(new Semaphore(0)),
+        m_isRunning(false),
+        m_thread(new std::thread([&](){ return this->Internal(); })) {
+            m_filePath = m_name + "-Profile.json";
+            // std::cout << "creating profile: " << m_name << std::endl;
+            m_isRunning = true;
+        }
         
     Session::Session(const std::string Name, const std::string FilePath) :
+        m_name(Name),
+        m_mtx(new std::mutex()),
+        m_sem(new Semaphore(0)),
         m_filePath(FilePath),
-        m_fileStream(m_filePath),
-        m_isRunning(true),
-        m_thread([&](){ return this->Internal(); }) {}
+        m_isRunning(false),
+        m_thread(new std::thread([&](){ return this->Internal(); })) {
+            // std::cout << "creating profile: " << m_name << std::endl;
+            m_isRunning = true;
+        }
     
     Session::~Session() {
-        m_isRunning = false;
-        m_cv.notify_all();
-        m_thread.join();
-        m_fileStream.flush();
-        m_fileStream.close();
+        // std::cout << "destroying profile: " << m_name << std::endl;
     }
     
     void Session::BeginInternal() {
+        while(!m_isRunning) std::_yield();
+        // if(m_fileStream) m_fileStream->std::~ofstream();
+        // std::cout << "session begin internal: " << m_name << std::endl;
+        m_fileStream = new std::ofstream(m_filePath);
         WriteHeader();
     }
     
     void Session::WriteHeader() {
-        m_fileStream.clear();
-        m_fileStream << std::setprecision(8) << std::fixed << "{\"otherData\": {},\"traceEvents\":[{}";
-		m_fileStream.flush();
+        m_fileStream->clear();
+        *m_fileStream << std::setprecision(8) << std::fixed << "{\"otherData\": {},\"traceEvents\":[{}";
+		m_fileStream->flush();
     }
     
     void Session::SaveResult(const Session::Result&& Result) {
-        std::CVThreadLock lock(m_mtx);
-        m_pendingResults.emplace_back(std::move(Result));
-        m_cv.notify_all();
+        if(m_isRunning) {
+            std::ThreadLock lock(*m_mtx);
+            m_pendingResults.emplace_back(std::move(Result));
+            m_sem->inc();
+        }
    };
 
     void Session::WriteResult(const Session::Result& Result) {
-        m_fileStream <<
+        // std::cout << "write result: " << m_name << std::endl;
+        *m_fileStream <<
         ",{" <<
         "\"cat\":\"function\"," <<
         "\"dur\":" << ADVClock::RuntimeCast<double>(Result.Elapsed, ADVClock::Precision::Microseconds) << ',' <<
@@ -49,35 +62,51 @@ namespace Profiler {
         "\"tid\":" << Result.ID << "," <<
         "\"ts\":" << ADVClock::RuntimeCast<double>(Result.Start, ADVClock::Precision::Microseconds) <<
         "}";
-        m_fileStream.flush();
+        m_fileStream->flush();
     }
     
     void Session::WriteFooter() {
-        m_fileStream << "]}";
-        m_fileStream.flush();
+        *m_fileStream << "]}";
+        m_fileStream->flush();
     }
     
     void Session::Internal() {
         BeginInternal();
+        Result R = { "" };
         while(m_isRunning) {
-            Result&& R = {};
-            {
-                std::CVThreadLock lock(m_mtx);
-                m_cv.wait(lock, [&] { return (m_pendingResults.size() > 0) || (!m_isRunning); });
-                if(m_isRunning){
-                    const bool tryL = m_mtx.try_lock();
-                    R = std::move(m_pendingResults[0]);
-                    m_pendingResults.pop_front();
-                    if(tryL) m_mtx.unlock();
-                }
+            m_sem->waitFor([&, this](const std::I64 cVal, const std::I64 CInitVal){
+                return (cVal != CInitVal) || (!m_isRunning);
+            });
+            std::ThreadLock lock(*m_mtx);
+            if(m_isRunning) {
+                if(m_pendingResults.size() > 0) InternalProcessFront(R);
             }
-            if(m_isRunning) WriteResult(R);
+            else {
+                // Flush
+                while(m_pendingResults.size() > 0) InternalProcessFront(R);
+                break;
+            }
         }
         EndInternal();
     }
     
+    void Session::InternalProcessFront(Session::Result& R) {
+        R = { "" };
+        R = std::move(m_pendingResults[0]);
+        m_pendingResults.pop_front();
+        WriteResult(R);
+        m_sem->dec();
+    }
+    
     void Session::EndInternal() {
         WriteFooter();
+        m_fileStream->close();
+        delete m_fileStream;
+        delete m_mtx;
+    }
+    
+    void Session::BeginSession(const std::string SessionName) {
+        SCM::CreateNewInstance(SessionName, SessionName);
     }
     
     void Session::BeginSession(const std::string SessionName, const std::string FilePath) {
@@ -85,7 +114,7 @@ namespace Profiler {
     }
     
     void Session::SaveResult(const std::string SessionName, Session::Result&& Result) {
-        SCM::GetInstanceByKey(SessionName, SessionName, SessionName + "-Profile.json").SaveResult(std::move(Result));
+        SCM::GetInstanceByKey(SessionName, SessionName).SaveResult(std::move(Result));
     }
     
     void Session::EndSession(const std::string SessionName) {
@@ -96,13 +125,33 @@ namespace Profiler {
         return SCM::Exists(SessionName);
     };
     
+    void Session::TerminateMain() {
+        for(auto& session : SCM::CMap)
+            Session::TerminateSession(session.first);
+    }
+    
+    void Session::TerminateSession(const std::string SessionName) {
+        auto& session = SCM::GetInstanceByKey(SessionName, SessionName);
+        session.m_isRunning = false;
+        session.m_sem->notify();
+        session.m_thread->join();
+    }
+    
     Session::ProfileScope::ProfileScope(const std::string SessionName, const std::string Name) :
         m_sessionName(SessionName),
-        m_result({ Name, std::this_thread::get_id(), (SessionExists(SessionName) ? SCM::GetInstanceByKey(SessionName, SessionName) : SCM::CreateNewInstance(SessionName, SessionName, SessionName + "-Profile.json")).m_timer.nowDur(), nanoseconds(0)}) { std::cout << "Profiling scope: " << Name << " using session: " << SessionName << std::endl; }
+        m_result({ Name, std::this_thread::get_id(), SCM::GetInstanceByKey(SessionName, SessionName).m_timer.nowDur(), nanoseconds(0)}) {
+        // std::cout << "Profiling scope: " << Name << " using session: " << SessionName << std::endl;
+    }
     
+    Session::ProfileScope::ProfileScope(Profiler::Session& session, const std::string Name) :
+        m_sessionName(SCM::GetKeyByInstance(session)),
+        m_result({ Name, std::this_thread::get_id(), session.m_timer.nowDur(), nanoseconds(0)}) { 
+        // std::cout << "Profiling scope: " << Name << " using session: " << SCM::GetKeyByInstance(session) << std::endl; 
+    }
+
     Session::ProfileScope::~ProfileScope() {
         const auto speed = SCM::GetInstanceByKey(m_sessionName, m_sessionName).m_timer.nowDur();
-        std::cout << "profiled and saving results: " << m_result.Name << std::endl;
+        // std::cout << "profiled and saving results: " << m_result.Name << std::endl;
         m_result.Elapsed = (speed - m_result.Start);
         Session::SaveResult(m_sessionName, std::move(m_result));
     }
--- profiler.h
+++ profiler.h
@@ -1,17 +1,20 @@
 #ifndef PROFILER_H_
 #define PROFILER_H_
 
+#include "vendor/portable-file-dialogs/portable-file-dialogs.h"
 #include <condition_variable>
 #include <deque>
 #include "vendor/STDExtras/STDExtras.hpp"
+#include "vendor/STDExtras/Semaphore/semaphore.h"
 #include "vendor/STDExtras/ADVClock/advclock.hpp"
 #include "vendor/STDExtras/vendor/Singleton/singleton_container_map.hpp"
+#include "vendor/FileUtilities/FileUtilities.hpp"
 
 using namespace std::this_thread;
 using namespace std::chrono;
 
-namespace Profiler {    
-    class Session : public SingletonContainerMap<Session> {
+namespace Profiler {
+    class Session : public SingletonContainerMap<Profiler::Session> {
         public:
         struct Result {
             std::string Name;
@@ -23,6 +26,7 @@ namespace Profiler {
         class ProfileScope {
             public:
             ProfileScope(const std::string SessionName, const std::string Name);
+            ProfileScope(Session& session, const std::string Name);
             ~ProfileScope();
             
             private:
@@ -31,10 +35,13 @@ namespace Profiler {
         };
         
         void SaveResult(const Session::Result&& Result);
+        static void BeginSession(const std::string SessionName);
         static void BeginSession(const std::string SessionName, const std::string FilePath);
         static void SaveResult(const std::string SessionName, Session::Result&& Result);
         static void EndSession(const std::string SessionName);
         static bool SessionExists(const std::string SessionName);
+        static void TerminateMain();
+        static void TerminateSession(const std::string SessionName);
         
         private:
         _SCM_CHILD_DECLORATIONS(Session)
@@ -46,16 +53,17 @@ namespace Profiler {
         void WriteHeader();
         void WriteResult(const Session::Result& Result);
         void WriteFooter();
+        void InternalProcessFront(Session::Result& R);
         void EndInternal();
         const std::string m_name;
-        const std::string m_filePath;
-        std::condition_variable m_cv;
-        std::mutex m_mtx;
-        std::ofstream m_fileStream;
-        std::thread m_thread;
+        FileUtilities::HLFileHandle m_filePath;
+        Semaphore* m_sem;
+        std::mutex* m_mtx;
+        std::ofstream* m_fileStream;
+        std::thread* m_thread;
         std::deque<Result> m_pendingResults;
         ADVClock m_timer;
-        bool m_isRunning;
+        bool m_isRunning = false;
         bool m_isblocking;
         friend class ProfileScope;
     };
